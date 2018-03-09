d09972328a4808793b7c771ec93e6b0b7a6fb426
dianavashti <diana.vashti@gmail.com>
2018-03-09 14:08:12 -0800

    Creates new view in admin portal for resources. Moves functionality to add new resource to this view.

--- client/Views/Admin/adminMain.js
+++ client/Views/Admin/adminMain.js
@@ -18,15 +18,6 @@ export default class AdminMain extends Component {
     super(props);
     this.state = {
       reports: [],
-      open: false,
-      anchorOrigin: {
-        horizontal: 'right',
-        vertical: 'bottom',
-      },
-      targetOrigin: {
-        horizontal: 'left',
-        vertical: 'bottom',
-      },
 			isLoading: true
     }
 
@@ -40,7 +31,7 @@ export default class AdminMain extends Component {
       headers: { 'x-auth': jwt }
     }
 
-    axios.get('https://st-james-bdl-api.herokuapp.com/api/admins/reports', config)
+    axios.get('http://localhost:8080/api/admins/reports', config)
       .then((response) => {
         this.setState({
           reports: response.data,
@@ -56,42 +47,9 @@ export default class AdminMain extends Component {
     this.fetchReports()
   }
 
-  handleTouchTap = (event) => {
-    // This prevents ghost click.
-    event.preventDefault();
-    this.setState({
-      open: true,
-      anchorEl: event.currentTarget,
-    });
-  };
-
-  handleRequestClose = () => {
-    this.setState({
-      open: false,
-    });
-  };
-
-  setAnchor = (positionElement, position) => {
-      const {anchorOrigin} = this.state;
-      anchorOrigin[positionElement] = position;
-
-      this.setState({
-        anchorOrigin: anchorOrigin,
-      });
-    };
-
-    setTarget = (positionElement, position) => {
-      const {targetOrigin} = this.state;
-      targetOrigin[positionElement] = position;
-
-      this.setState({
-        targetOrigin: targetOrigin,
-      });
-    };
-
   logOut() {
     axios.defaults.headers.common['x-auth'] = sessionStorage.getItem('auth')
-    axios.delete('https://st-james-bdl-api.herokuapp.com/api/admins/logout')
+    axios.delete('http://localhost:8080/api/admins/logout')
       .then(() => {
         browserHistory.push('/')
         sessionStorage.clear()
@@ -109,29 +67,20 @@ export default class AdminMain extends Component {
       <div>
         <ReportsTable reports={this.state.reports} fetchReports={this.fetchReports} />
         <RaisedButton
-          label="Add Resource"
-          primary={false}
-          backgroundColor="#C8C5C5"
-          labelColor="#090200"
-          onTouchTap={this.handleTouchTap}
-          style={{margin: "5"}}
-        />
-          <Popover
-            open={this.state.open}
-            anchorEl={this.state.anchorEl}
-            anchorOrigin={this.state.anchorOrigin}
-            targetOrigin={this.state.targetOrigin}
-            onRequestClose={this.handleRequestClose}
-            style={{padding: 5, width: "50%", boxShadow: "3px 3px 3px #E0E0E0", border: "1px solid #E0E0E0"}}>
-            <AdminResourceForm />
-          </Popover>
-        <RaisedButton
           label="Log Out"
           primary={false}
           backgroundColor="#E21E26"
           labelColor="#FAFAFA"
           onTouchTap={this.logOut}
         />
+        <Link to="/admin-resources"><RaisedButton
+          label="Edit Resources"
+          secondary={true}
+          backgroundColor="#E21E26"
+          labelColor="#FAFAFA"
+          onTouchTap={this.preventGhostClick}
+          style={{margin: "5"}}
+        /></Link>
       </div>
     )
   }
